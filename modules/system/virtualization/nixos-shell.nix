c7db87f469c9cbc80d1965c3825c92ac573adf90
Aidan Wright <38870143+AidanWright@users.noreply.github.com>
2026-06-01 02:59:32 -0400

    feat(nixos-shell): add nixos-shell linux VM runner

---
+++ modules/system/virtualization/nixos-shell.nix
@@ -0,0 +1,20 @@
+# modules/system/virtualization/nixos-shell.nix
+################################################################################
+# Exposes nix run --impure .#nixosConfigurations.<host>.config.system.build.nixos-shell
+# --impure is required: reads $SHELL and $HOME to mount the host's home directory
+# into the VM, and detects the host platform for Darwin compatibility.
+# See: https://github.com/Mic92/nixos-shell
+################################################################################
+{ ... }:
+{
+  flake-file.inputs.nixos-shell = {
+    url = "github:Mic92/nixos-shell";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  flake.modules.nixos.nixos-shell =
+    { inputs, ... }:
+    {
+      imports = [ inputs.nixos-shell.nixosModules.nixos-shell ];
+    };
+}
